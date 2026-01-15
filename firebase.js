07f9dd8a12c08ab0d12f3efe31eee9db7d9a6fb6
José Murilo Lopes <josemurilolopes11052008@gmail.com>
2026-01-15 10:51:29 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,17 @@
-// Importa as funções necessárias do Firebase
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Configuração do Firebase (use a SUA, não essa de exemplo)
- const firebaseConfig = {
-    apiKey: "AIzaSyB7ak6tWGgC9-fRJNY8GoHBpIB-FMPVIio",
-    authDomain: "fja---plataform.firebaseapp.com",
-    projectId: "fja---plataform",
-    storageBucket: "fja---plataform.firebasestorage.app",
-    messagingSenderId: "165691485924",
-    appId: "1:165691485924:web:cfb4a4b453d53480eec6f1"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyB7ak6tWGgC9-fRJNY8GoHBpIB-FMPVIio",
+  authDomain: "fja---plataform.firebaseapp.com",
+  projectId: "fja---plataform",
+  storageBucket: "fja---plataform.appspot.com",
+  messagingSenderId: "165691485924",
+  appId: "1:165691485924:web:cfb4a4b453d53480eec6f1"
+};
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializa o Firestore
 const db = getFirestore(app);
 
-// Exporta o banco para outros arquivos usarem
 export { db };
